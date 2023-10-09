6ce8ba459744bb723baf3cb4a4534a79cddc34e9
saqaro <147359908+saqaro@users.noreply.github.com>
2023-10-08 21:52:54 -0500

    Update line-mini.tsx
    Signed-off-by: saqaro <147359908+saqaro@users.noreply.github.com>

--- components/progress/demo/line-mini.tsx
+++ components/progress/demo/line-mini.tsx
@@ -3,8 +3,8 @@ import { Progress } from 'antd';
 
 const App: React.FC = () => (
   <div style={{ width: 170 }}>
-    <Progress percent={30} size="small" />
-    <Progress percent={50} size="small" status="active" />
+    <Progress percent={74} size="small" />
+    <Progress percent={54} size="small" status="active" />
     <Progress percent={70} size="small" status="exception" />
     <Progress percent={100} size="small" />
   </div>
